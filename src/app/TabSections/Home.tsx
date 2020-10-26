4c6462e7a9e3b13b9c9df659584e8e5c93034714
Christie Molloy <cmolloy@redhat.com>
2020-10-26 17:01:59 -0400

    add cards

--- src/app/TabSections/Home.tsx
+++ src/app/TabSections/Home.tsx
@@ -1,13 +1,58 @@
 import * as React from 'react';
-import { 
-  PageSection,
-  Title 
+import {
+  Card,
+  CardHeader,
+  CardHeaderMain,
+  CardTitle,
+  CardBody,
+  Gallery,
+  GalleryItem
 } from '@patternfly/react-core';
 
-const Home: React.FunctionComponent = () => (
-  <>
-    <Title headingLevel="h1" size="lg">Home component - cards go here</Title>
-  </>
-)
+const Home: React.FunctionComponent = () => {
+
+  const cardDetails = [
+    {
+      "title":"Kafka basics",
+      "description": "Learn the basics of Apache Kafka, the heart of Strimzi, in 6 steps"
+    },
+    {
+      "title":"Create a topic",
+      "description": "Topics are a named stream of messages. You can create a topic using the UI."
+    },
+    {
+      "title":"Documentation",
+      "description": "View product documentations, as well as Quickstarts and community content."
+    },
+    {
+      "title":"Connect to this cluster",
+      "description": "Get connection details and sample code to connect your applications."
+    }
+  ];
+
+  return (
+    <>
+      <Gallery hasGutter>
+        { cardDetails.map(cardDetail => (
+          <GalleryItem>
+            <Card>
+              <CardHeader>
+                <CardHeaderMain>
+                  {/* <Brand src={pfLogo} alt="PatternFly logo" style={{ height: '50px' }}/> */}
+                </CardHeaderMain>
+              </CardHeader> 
+              <CardTitle>
+                {cardDetail.title}
+              </CardTitle>
+              <CardBody>
+                {cardDetail.description}
+              </CardBody>
+            </Card>
+          </GalleryItem>
+        ))}
+      </Gallery>
+    </>
+  )
+}
 
 export { Home };
